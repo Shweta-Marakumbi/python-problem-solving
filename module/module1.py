1d4fcaf35559f9fdcfbddf2046aeee7d0c5057c3
ShwetaSM <swetamarkumbi@gmail.com>
2026-03-29 19:56:58 +0530

    Add files via upload

---
+++ module/module1.py
@@ -0,0 +1,11 @@
+def welcome(fname, lname):
+    print("hello," +fname)
+    print("hello," +lname)
+def login(username, password):
+    if username == password:
+        print("You have login successfully")
+    else:
+        print("you have entered wrong username and password")
+def square(num):
+    print("square of two no=", num*num)
+pi=3.1415926535898793
---
+++ module/module2.py
@@ -0,0 +1,26 @@
+# # first way to import module 1
+# import module1
+# module1.square(4)
+# module1.login("admin","abc")
+# print(module1.pi)
+
+##second way is to import a module as mod
+# import module1 as mod 
+# mod.login('user','use')
+# print(mod.pi)
+# mod.square(4)
+
+# #third way to import a specific func/var/class/etc
+# from module1 import pi,square,login,welcome
+# print(pi)
+# square(4)
+# login('user','user')
+# welcome('shweta','marakumbi')
+
+# #4th way if you are looking to import everything(all)
+# from module1 import *
+# print(pi)
+# square(4)
+# login('user','user')
+# welcome('shweta','marakumbi')
+
